d72bbcde233db1045a81da4850bb66fec9c00e7b
Vadim Filatov <default87@gmail.com>
2019-03-16 23:32:09 +0100

    simple ttl mechanic

--- engine/narwal/engine.go
+++ engine/narwal/engine.go
@@ -5,6 +5,7 @@ import (
 	"regexp"
 	"strings"
 	"sync"
+	"time"
 
 	"github.com/filatovw/ni-storage/engine"
 	"github.com/filatovw/ni-storage/logger"
@@ -12,10 +13,63 @@ import (
 )
 
 type Narwal struct {
-	log  logger.Logger
-	lock *sync.RWMutex
-	data map[string]engine.Record
-	wal  *WAL
+	log    logger.Logger
+	lock   *sync.RWMutex
+	data   map[string]engine.Record
+	wal    *WAL
+	ttlIdx *TTLIdx
+}
+
+type TTLIdx struct {
+	stack []ttlRecord
+}
+
+type ttlRecord struct {
+	until time.Time
+	key   string
+}
+
+func (idx *TTLIdx) Push(r ttlRecord) {
+	if len(idx.stack) == 0 {
+		idx.stack = append(idx.stack, r)
+		return
+	}
+
+	i := 0
+	for _, e := range idx.stack {
+		if r.until.After(e.until) {
+			i++
+			break
+		}
+	}
+	idx.stack = append(idx.stack[:i], append([]ttlRecord{r}, idx.stack[i:]...)...)
+}
+
+func (idx *TTLIdx) Delete(k string) {
+	i := 0
+	for _, e := range idx.stack {
+		if e.key == k {
+			break
+		}
+		i++
+	}
+	copy(idx.stack[i:], idx.stack[i+1:])
+	idx.stack = idx.stack[:len(idx.stack)-1]
+}
+
+func (idx *TTLIdx) PopAfter(t time.Time) []string {
+	results := []string{}
+	i := 0
+	for _, e := range idx.stack {
+		if e.until.Before(t) {
+			results = append(results, e.key)
+		}
+		i++
+	}
+	if i > 0 {
+		idx.stack = idx.stack[i-1:]
+	}
+	return results
 }
 
 type event struct {
@@ -37,12 +91,30 @@ func New(path string, log logger.Logger) (*Narwal, error) {
 		return nil, err
 	}
 
-	return &Narwal{
-		log:  log,
-		wal:  wal,
-		lock: &sync.RWMutex{},
-		data: snapshot,
-	}, nil
+	storage := &Narwal{
+		log:    log,
+		wal:    wal,
+		lock:   &sync.RWMutex{},
+		data:   snapshot,
+		ttlIdx: &TTLIdx{stack: []ttlRecord{}},
+	}
+
+	go func() {
+		t := time.NewTicker(time.Second * 3)
+		for {
+			select {
+			case <-t.C:
+				keys := storage.ttlIdx.PopAfter(time.Now())
+				log.Infof("keys: %s", keys)
+				for _, key := range keys {
+					log.Infof("Removed by TTL: %s", key)
+					storage.Delete(key)
+				}
+			}
+		}
+	}()
+
+	return storage, nil
 }
 
 func (s *Narwal) Exists(key string) bool {
@@ -117,12 +189,18 @@ func (s *Narwal) set(record engine.Record) {
 	if err := s.wal.Write(event{Record: record, Action: actionSet}); err != nil {
 		s.log.Error(err)
 	}
+	if record.ExpirationTime != nil {
+		s.ttlIdx.Push(ttlRecord{key: record.Key, until: *record.ExpirationTime})
+	}
 	s.data[record.Key] = record
+	s.log.Infof("data %v", s.data)
+	s.log.Infof("ttl %v", s.ttlIdx)
 }
 
 func (s *Narwal) delete(key string) {
 	if err := s.wal.Write(event{Record: engine.Record{Key: key}, Action: actionDelete}); err != nil {
 		s.log.Error(err)
 	}
+	s.ttlIdx.Delete(key)
 	delete(s.data, key)
 }
